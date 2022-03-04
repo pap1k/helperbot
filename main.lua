7d4845d96c4645cfb6c520c79362ef8a045a850c
popka <magemaster555@gmail.com>
2022-03-04 06:38:03 +0300

    Фикс ловеркейса, добавил сброс кд через 5 сек, написал говно

--- main.lua
+++ main.lua
@@ -24,8 +24,12 @@ playerid = -1
 
 function main()
     repeat wait(0) until isSampAvailable()
-    -- че это
+
     sampAddChatMessage("{e9ab4f}[HH]{5fdbea} HelperBot Loaded. Ебаш.", -1)
+
+    sampRegisterChatCommand("test", function(txt)
+        sampAddChatMessage(lower(txt), -1)
+    end)
 
     sampRegisterChatCommand('hh', function()
 		winActive.v = not winActive.v
@@ -34,6 +38,7 @@ function main()
 
     while true do 
         wait(0)
+        gui.imgui.ShowCursor = winActive.v
         if settings.get("useAnswId") then
             if playerid ~= -1 then
                 if wasKeyPressed(settings.get("btnAnswId")) then
@@ -155,15 +160,14 @@ end
 function findAnswer(inputstr, id)
     local work = true
     while work do
-        local result = search(u8(inputstr:lower()))
-        print("RESULT", result, "SUB", result.sub(1, 2))
+        local result = search(u8(lower(inputstr)))
         if result == "!!" then
             work = false
         elseif result == "!!wait" then
             wait(100)
         else
             work = false
-            if answer.status then
+            if answer.status and answer.ts + 5 >= os.clock() then
                 answer.ts = os.clock()
             end
             while answer.ts + 0.5 >= os.clock() do
@@ -192,6 +196,11 @@ function search(inputstr)
         for k, variant in ipairs(b) do
             local count = 0
             for i = 1, #variant.trig do
+                print(variant.trig[i], inputstr)
+                print(inputstr:match("%s"..variant.trig[i].."%s"),
+                inputstr:match("%s"..variant.trig[i].."$"),
+                inputstr:match("^"..variant.trig[i].."%s"),
+                inputstr:match("^"..variant.trig[i].."$"))
                 if  inputstr:match("%s"..variant.trig[i].."%s") or 
                     inputstr:match("%s"..variant.trig[i].."$") or 
                     inputstr:match("^"..variant.trig[i].."%s") or 
@@ -213,4 +222,25 @@ function search(inputstr)
     else
         return '!!wait'
     end
+end
+
+function lower(s)
+    local russian_characters = {
+        [168] = 'Ё', [184] = 'ё', [192] = 'А', [193] = 'Б', [194] = 'В', [195] = 'Г', [196] = 'Д', [197] = 'Е', [198] = 'Ж', [199] = 'З', [200] = 'И', [201] = 'Й', [202] = 'К', [203] = 'Л', [204] = 'М', [205] = 'Н', [206] = 'О', [207] = 'П', [208] = 'Р', [209] = 'С', [210] = 'Т', [211] = 'У', [212] = 'Ф', [213] = 'Х', [214] = 'Ц', [215] = 'Ч', [216] = 'Ш', [217] = 'Щ', [218] = 'Ъ', [219] = 'Ы', [220] = 'Ь', [221] = 'Э', [222] = 'Ю', [223] = 'Я', [224] = 'а', [225] = 'б', [226] = 'в', [227] = 'г', [228] = 'д', [229] = 'е', [230] = 'ж', [231] = 'з', [232] = 'и', [233] = 'й', [234] = 'к', [235] = 'л', [236] = 'м', [237] = 'н', [238] = 'о', [239] = 'п', [240] = 'р', [241] = 'с', [242] = 'т', [243] = 'у', [244] = 'ф', [245] = 'х', [246] = 'ц', [247] = 'ч', [248] = 'ш', [249] = 'щ', [250] = 'ъ', [251] = 'ы', [252] = 'ь', [253] = 'э', [254] = 'ю', [255] = 'я',
+      }
+    local strlen = s:len()
+    if strlen == 0 then return s end
+    s = s:lower()
+    local output = ''
+    for i = 1, strlen do
+        local ch = s:byte(i)
+        if ch >= 192 and ch <= 223 then -- upper russian characters
+            output = output .. russian_characters[ch+32]
+        elseif ch == 168 then -- Ё
+            output = output .. russian_characters[184]
+        else
+            output = output .. string.char(ch)
+        end
+    end
+    return output
 end
